02b53f0fc9cb7c277d2b21af302af030f32876fa
Richard Trávníček <89924371+Hadrik@users.noreply.github.com>
2026-02-28 21:40:09 +0100

    Auto start

---
+++ StarteR/Services/AutoStartService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace StarteR.Services;
+
+public class AutoStartService
+{
+    public const string AppName = "StarteR";
+    
+    public static bool AddToStartup(string?[] args)
+    {
+        return AddToStartup(AppName, System.Reflection.Assembly.GetExecutingAssembly().Location, args);
+    }
+    
+    public static bool AddToStartup(string appName, string appPath, string?[] args)
+    {
+        try
+        {
+            var rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            rk?.SetValue(appName, $"\"{appPath}\" {string.Join(" ", args)}", RegistryValueKind.String);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static bool RemoveFromStartup(string appName = AppName)
+    {
+        try
+        {
+            var rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            rk?.DeleteValue(appName, true);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    public static bool IsInStartup(string appName = AppName, bool removeOldValue = true)
+    {
+        try
+        {
+            var rk = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", false);
+            var value = rk?.GetValue(appName)?.ToString();
+            if (value == null) return false;
+            
+            if (removeOldValue && StripArgs(value) != System.Reflection.Assembly.GetExecutingAssembly().Location)
+            {
+                RemoveFromStartup(appName);
+                return false;
+            }
+            
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    private static string StripArgs(string value)
+    {
+        if (value.StartsWith('\"'))
+        {
+            var endQuoteIndex = value.IndexOf('"', 1);
+            if (endQuoteIndex > 0)
+            {
+                return value[1..endQuoteIndex];
+            }
+        }
+        else
+        {
+            var firstSpaceIndex = value.IndexOf(' ');
+            if (firstSpaceIndex > 0)
+            {
+                return value[..firstSpaceIndex];
+            }
+        }
+
+        return value;
+    }
+}
--- StarteR/ViewModels/SettingsViewModel.cs
+++ StarteR/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace StarteR.ViewModels;
+﻿using StarteR.Services;
+
+namespace StarteR.ViewModels;
 
 public class SettingsViewModel : ViewModelBase
 {
-    
+    public bool IsAutoStartEnabled
+    {
+        get => AutoStartService.IsInStartup();
+        set
+        {
+            if (value)
+            {
+                AutoStartService.AddToStartup(["--run"]);
+            }
+            else
+            {
+                AutoStartService.RemoveFromStartup();
+            }
+            OnPropertyChanged();
+        }
+    }
 }
